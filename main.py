4fbae0d079394f68f64f93427127484d0a5847a5
Preston Locke <locke_preston@yahoo.com>
2019-12-03 17:19:01 -0600

    Added empty action bar at the top of the window

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import pygame
 
 # Constants to define the scale of our tiles and screen
 DIVIDER_WIDTH = 1
+ACTION_BAR_HEIGHT = 40
 CELL_WIDTH = 20
 CELL_HEIGHT = 20
 CELL_COUNT_X = 32
 CELL_COUNT_Y = 32
 
-# TODO: Add calculations for menu/actions bar at the top of the screen
 # Calculate the actual pixel dimensions of our window
 WINDOW_WIDTH = CELL_WIDTH * CELL_COUNT_X + DIVIDER_WIDTH * (CELL_COUNT_X - 1)
-WINDOW_HEIGHT = CELL_HEIGHT * CELL_COUNT_Y + DIVIDER_WIDTH * (CELL_COUNT_Y - 1)
+WINDOW_HEIGHT = CELL_HEIGHT * CELL_COUNT_Y + DIVIDER_WIDTH * (CELL_COUNT_Y - 1) + ACTION_BAR_HEIGHT
 
 # TODO: Pick good cell colors
 # Color scheme constants
 # noinspection PyArgumentList
-CELL_COLOR_EMPTY = pygame.Color(255, 255, 255)
+ACTION_BAR_COLOR = pygame.Color(255, 255, 102)
 # noinspection PyArgumentList
-CELL_COLOR_WALL = pygame.Color(0, 0, 0)
+CELL_COLOR_EMPTY = pygame.Color(240, 240, 240)
+# noinspection PyArgumentList
+CELL_COLOR_WALL = pygame.Color(60, 60, 60)
 # noinspection PyArgumentList
 CELL_COLOR_EXPLORED = pygame.Color(0, 255, 0)
 CELL_COLOR_START = CELL_COLOR_EXPLORED
@@ -32,13 +34,13 @@ class Level(object):
         with open(file_name) as level_file:
             data = json.load(level_file)
             # A 2D array of booleans to signify where the walls are in the level
-            walls = data['walls']
+            walls = data["walls"]
             self.cells = []
             for x in range(0, CELL_COUNT_X):
                 column = []
                 for y in range(0, CELL_COUNT_Y):
                     window_x = x * (CELL_WIDTH + DIVIDER_WIDTH)
-                    window_y = y * (CELL_HEIGHT + DIVIDER_WIDTH)
+                    window_y = y * (CELL_HEIGHT + DIVIDER_WIDTH) + ACTION_BAR_HEIGHT
                     rect = pygame.Rect(window_x, window_y, CELL_WIDTH, CELL_HEIGHT)
                     column.append(Cell(rect, x, y, walls[x][y]))
                 self.cells.append(column)
@@ -95,12 +97,24 @@ class Cell(object):
         self.neighbors.append(neighbor)
 
     def __str__(self):
-        return "("+str(self.x)+", "+str(self.y)+")"
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+
+class ActionBar(object):
+    def __init__(self):
+        self.rect = pygame.Rect((0, 0, WINDOW_WIDTH, ACTION_BAR_HEIGHT))
+        self.color = ACTION_BAR_COLOR
+
+    def render(self, surface: pygame.Surface):
+        return pygame.draw.rect(surface, self.color, self.rect)
 
 
 def main():
     pygame.init()
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # Initialize the action bar
+    action_bar = ActionBar()
 
     # Initialize the default level
     level = Level()
@@ -113,6 +127,7 @@ def main():
 
         # Render frame to screen
         updates = level.render(window)
+        updates.append(action_bar.render(window))
         pygame.display.update(updates)
 
         # Wait 30 milliseconds between frames
